75d4dd7ad54b278d956a56e34ea2e256e192e3bd
fabinho1992 <f.santosdev1992@gmail.com>
2026-06-23 22:02:46 -0300

    Adicionando arquivo Dokerfile para o render

--- RotaCerta.API/Program.cs
+++ RotaCerta.API/Program.cs
@@ -5,6 +5,8 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://+:8080");
+
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
